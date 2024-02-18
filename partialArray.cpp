a1d420fe641afe8be262e3a57e75e2d51cb91900
Jeka34G <157092323+Jeka34G@users.noreply.github.com>
2024-02-18 15:11:13 -0800

    Update partialArray.cpp

--- partialArray.cpp
+++ partialArray.cpp
@@ -6,7 +6,7 @@ int main() {
 	int array[SIZE]; // array declaration
 
 	int numValues = 0; // The number of items stored in the array
-					   // Initialized to zero because there is no values stored YET :)
+			   // Initialized to zero because there is no values stored YET :)
 
 	int number;
 
@@ -17,7 +17,7 @@ int main() {
 											   // By doing so, the user can exit early and have the array partially filled.
 
 		array[numValues] = number; // store the current number input by the user INTO the array	at the 'numValues' position
-		numValues++;			   // Incremented to track how many numbers were entered AND to move to the next position.
+		numValues++;	           // Incremented to track how many numbers were entered AND to move to the next position.
 
 		cout << "Enter a number or -1 to quit: "; // Prompt the user again
 		cin >> number;
@@ -27,7 +27,7 @@ int main() {
 	cout << "Like this: ";
 
 	for (int counter = 0; counter <= numValues; counter++) { // This for loop can be simplified, 
-															 // although it is better in terms of independence, it can be put into a function easily!
+								 // although it is better in terms of independence, it can be put into a function easily!
 		if (counter == 0)
 			cout << "{" << array[counter] << ",";
 		else if (counter == numValues)
